ae5dbb1326b6a8af0b16fdafbabd590239bfda66
AndyZhou952 <jzhoubc@gmail.com>
2026-04-22 08:02:45 +0000

    render doc, workflow

---
+++ docs/conf.py
@@ -0,0 +1,51 @@
+# Copyright 2026 verl-omni contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Configuration file for the Sphinx documentation builder.
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+project = "verl-omni"
+copyright = "2026 verl-omni contributors"
+author = "verl-omni contributors"
+
+master_doc = "index"
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+]
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+language = "en"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+
+suppress_warnings = ["ref.duplicate", "ref.myst"]
