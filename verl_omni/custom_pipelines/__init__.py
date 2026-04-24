5966b1ef3557648427be70af6c1f0840f2f471c0
Mike Cheung <zhtmike@gmail.com>
2026-04-24 16:14:04 +0800

    fix CI

--- verl_omni/custom_pipelines/__init__.py
+++ verl_omni/custom_pipelines/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import qwen_image
+from .qwen_image import *  # noqa: F401, F403
 
 __all__ = qwen_image.__all__
