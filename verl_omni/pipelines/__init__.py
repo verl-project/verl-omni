dfd504f2285896898dc718dfe7134d273c862c4e
princepride <wangzhipeng628@gmail.com>
2026-05-11 10:03:49 +0000

    xxx
    Signed-off-by: Wang, Zhipeng | RASIA <zhipeng.wang@rakuten.com>

--- verl_omni/pipelines/__init__.py
+++ verl_omni/pipelines/__init__.py
@@ -20,4 +20,5 @@ from . import (
 from .bagel_flow_grpo import *  # noqa: F401, F403
 from .qwen_image_flow_grpo import *  # noqa: F401, F403
 
-__all__ = list(qwen_image_flow_grpo.__all__) + list(bagel_flow_grpo.__all__)
+__all__ = list(qwen_image_flow_grpo.__all__) 
+__all__ += list(bagel_flow_grpo.__all__)
