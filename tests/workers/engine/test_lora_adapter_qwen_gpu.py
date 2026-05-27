41552760914e32218c4fadf2b82eefe2d555db49
AndyZhou952 <jzhoubc@connect.ust.hk>
2026-05-27 18:05:49 +0800

    clean

--- tests/workers/engine/test_lora_adapter_qwen_gpu.py
+++ tests/workers/engine/test_lora_adapter_qwen_gpu.py
@@ -82,7 +82,7 @@ def test_use_adapter_switches_active_adapter():
     model_path = _require_model_path()
     engine, module = _make_qwen_engine(model_path)
 
-    assert module.active_adapter == "default"
+    assert list(module.active_adapters) == ["default"]
     with engine.use_adapter("old"):
-        assert module.active_adapter == "old"
-    assert module.active_adapter == "default"
+        assert list(module.active_adapters) == ["old"]
+    assert list(module.active_adapters) == ["default"]
