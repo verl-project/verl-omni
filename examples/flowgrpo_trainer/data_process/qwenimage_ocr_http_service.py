b139f951b14cbc0601b956d9d8083ab1af62ccce
Ruihang Li <ruihanglix@gmail.com>
2026-05-28 11:40:01 +0800

    [misc] chore: add license headers and formatting to http scorer files

--- examples/flowgrpo_trainer/data_process/qwenimage_ocr_http_service.py
+++ examples/flowgrpo_trainer/data_process/qwenimage_ocr_http_service.py
@@ -1,3 +1,17 @@
+# Copyright 2026 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Preprocess the OCR dataset to parquet format for HTTP scorer service training.
 
@@ -13,11 +27,12 @@ import os
 
 import datasets
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", default="~/dataset/ocr/", help="Path to the raw OCR dataset directory.")
-    parser.add_argument("--output_dir", default="~/data/ocr_http", help="Directory to save the preprocessed parquet files.")
+    parser.add_argument(
+        "--output_dir", default="~/data/ocr_http", help="Directory to save the preprocessed parquet files."
+    )
     args = parser.parse_args()
 
     local_dataset_path = os.path.expanduser(args.input_dir)
@@ -48,6 +63,7 @@ if __name__ == "__main__":
                 "extra_info": {"split": split, "index": idx},
             }
             return data
+
         return process_fn
 
     train_dataset = dataset["train"].map(function=make_map_fn("train"), with_indices=True)
