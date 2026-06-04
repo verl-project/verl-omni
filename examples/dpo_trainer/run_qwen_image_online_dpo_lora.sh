8a97b44b1b774e908622fb1394e2fec9983df698
Didan Deng <33117903+wtomin@users.noreply.github.com>
2026-06-04 16:24:10 +0800

    Revert "change micro mini bsz to 16"
    This reverts commit 945654207a583c83f1d1509c2296d339ad13a841.

--- examples/dpo_trainer/run_qwen_image_online_dpo_lora.sh
+++ examples/dpo_trainer/run_qwen_image_online_dpo_lora.sh
@@ -40,7 +40,7 @@ python3 -m verl_omni.trainer.main_diffusion \
     actor_rollout_ref.actor.optim.lr=3e-4 \
     actor_rollout_ref.actor.optim.weight_decay=0.0001 \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.actor.fsdp_config.model_dtype=bfloat16 \
