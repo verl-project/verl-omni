089561a4ae5410a088a83d3927f600591db6e603
zihaowang26 <wangzihao111@huawei.com>
2026-06-09 19:08:43 +0800

    fix instruction

--- verl_omni/utils/reward_score/hpsv3_reward.py
+++ verl_omni/utils/reward_score/hpsv3_reward.py
@@ -29,51 +29,45 @@ logger.setLevel(os.getenv("VERL_LOGGING_LEVEL", "INFO"))
 _lock = threading.Lock()
 _inferencer = None
 
-_INSTRUCTION = """
-You are tasked with evaluating a generated image based on Visual Quality and Text 
-Alignment and give a overall score to estimate the human preference. Please provide 
-a rating from 0 to 10, with 0 being the worst and 10 being the best.
-
-**Visual Quality:**
-Evaluate the overall visual quality of the image. The following sub-dimensions 
-should be considered:
-- **Reasonableness:** The image should not contain any significant biological or 
-  logical errors, such as abnormal body structures or nonsensical environmental 
-  setups.
-- **Clarity:** Evaluate the sharpness and visibility of the image. The image 
-  should be clear and easy to interpret, with no blurring or indistinct areas.
-- **Detail Richness:** Consider the level of detail in textures, materials, 
-  lighting, and other visual elements (e.g., hair, clothing, shadows).
-- **Aesthetic and Creativity:** Assess the artistic aspects of the image, 
-  including the color scheme, composition, atmosphere, depth of field, and the 
-  overall creative appeal. The scene should convey a sense of harmony and balance.
-- **Safety:** The image should not contain harmful or inappropriate content, 
-  such as political, violent, or adult material. If such content is present, the 
-  image quality and satisfaction score should be the lowest possible.
-
-**Text Alignment:**
-Assess how well the image matches the textual prompt across the following 
-sub-dimensions:
-- **Subject Relevance** Evaluate how accurately the subject(s) in the image 
-  (e.g., person, animal, object) align with the textual description. The subject 
-  should match the description in terms of number, appearance, and behavior.
-- **Style Relevance:** If the prompt specifies a particular artistic or stylistic 
-  style, evaluate how well the image adheres to this style.
-- **Contextual Consistency**: Assess whether the background, setting, and 
-  surrounding elements in the image logically fit the scenario described in the 
-  prompt. The environment should support and enhance the subject without 
-  contradictions.
-- **Attribute Fidelity**: Check if specific attributes mentioned in the prompt 
-  (e.g., colors, clothing, accessories, expressions, actions) are faithfully 
-  represented in the image. Minor deviations may be acceptable, but critical 
-  attributes should be preserved.
-- **Semantic Coherence**: Evaluate whether the overall meaning and intent of the 
-  prompt are captured in the image. The generated content should not introduce 
-  elements that conflict with or distort the original description.
-Textual prompt - {text_prompt}
-
-
-"""
+_INSTRUCTION = (
+    "\nYou are tasked with evaluating a generated image based on Visual Quality and Text"
+    " Alignment and give a overall score to estimate the human preference. Please provide"
+    " a rating from 0 to 10, with 0 being the worst and 10 being the best. \n"
+    "\n"
+    "**Visual Quality:**  \n"
+    "Evaluate the overall visual quality of the image. The following sub-dimensions should be considered:\n"
+    "- **Reasonableness:** The image should not contain any significant biological or logical errors,"
+    " such as abnormal body structures or nonsensical environmental setups.\n"
+    "- **Clarity:** Evaluate the sharpness and visibility of the image. The image should be clear and"
+    " easy to interpret, with no blurring or indistinct areas.\n"
+    "- **Detail Richness:** Consider the level of detail in textures, materials, lighting, and other"
+    " visual elements (e.g., hair, clothing, shadows).\n"
+    "- **Aesthetic and Creativity:** Assess the artistic aspects of the image, including the color"
+    " scheme, composition, atmosphere, depth of field, and the overall creative appeal. The scene should"
+    " convey a sense of harmony and balance.\n"
+    "- **Safety:** The image should not contain harmful or inappropriate content, such as political,"
+    " violent, or adult material. If such content is present, the image quality and satisfaction score"
+    " should be the lowest possible. \n"
+    "\n"
+    "**Text Alignment:**  \n"
+    "Assess how well the image matches the textual prompt across the following sub-dimensions:\n"
+    "- **Subject Relevance** Evaluate how accurately the subject(s) in the image (e.g., person, animal,"
+    " object) align with the textual description. The subject should match the description in terms of"
+    " number, appearance, and behavior.\n"
+    "- **Style Relevance:** If the prompt specifies a particular artistic or stylistic style, evaluate"
+    " how well the image adheres to this style.\n"
+    "- **Contextual Consistency**: Assess whether the background, setting, and surrounding elements in"
+    " the image logically fit the scenario described in the prompt. The environment should support and"
+    " enhance the subject without contradictions.\n"
+    "- **Attribute Fidelity**: Check if specific attributes mentioned in the prompt (e.g., colors,"
+    " clothing, accessories, expressions, actions) are faithfully represented in the image. Minor"
+    " deviations may be acceptable, but critical attributes should be preserved.\n"
+    "- **Semantic Coherence**: Evaluate whether the overall meaning and intent of the prompt are"
+    " captured in the image. The generated content should not introduce elements that conflict with or"
+    " distort the original description.\n"
+    "Textual prompt - {text_prompt}\n"
+    "\n\n"
+)
 
 _PROMPT_WITH_SPECIAL_TOKEN = """
 Please provide the overall ratings of this image: <|Reward|>
