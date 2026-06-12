9d7588a6565ee946f3e4df4f8dc160f122831a1c
samithuang <285365963@qq.com>
2026-06-12 07:22:27 +0000

    linting
    Signed-off-by: samithuang <285365963@qq.com>

--- tests/workers/test_diffusers_fsdp_engine.py
+++ tests/workers/test_diffusers_fsdp_engine.py
@@ -90,6 +90,7 @@ def create_training_config(model_type, strategy, device_count, model, policy_sta
         "algo.sde_type=sde",
     ]
     from verl_omni.utils.diffusion_attention import fa3_available
+
     if cp > 1 or not fa3_available():
         model_overrides.append("attn_backend=native")
     if policy_state_adapters is not None:
