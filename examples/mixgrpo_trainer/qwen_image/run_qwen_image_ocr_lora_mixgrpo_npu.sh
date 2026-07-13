866503061fbed85df36b3bb381c2a612e297dd94
Trigger <Meng.Bo.Wang@outlook.com>
2026-07-13 15:18:15 +0800

    Enable NumPy serialization in Qwen-Image NPU examples

--- examples/mixgrpo_trainer/qwen_image/run_qwen_image_ocr_lora_mixgrpo_npu.sh
+++ examples/mixgrpo_trainer/qwen_image/run_qwen_image_ocr_lora_mixgrpo_npu.sh
@@ -4,6 +4,7 @@
 #            https://github.com/Tencent-Hunyuan/MixGRPO
 #
 set -x
+export VERL_DATAPROTO_SERIALIZATION_METHOD=numpy
 ASCEND_HOME_PATH=${ASCEND_HOME_PATH:-/usr/local/Ascend/cann-9.0.0}
 source $ASCEND_HOME_PATH/set_env.sh
 source $ASCEND_HOME_PATH/../nnal/atb/set_env.sh
