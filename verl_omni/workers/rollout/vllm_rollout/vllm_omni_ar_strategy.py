5859522baaa190f5ae5bb4078b92d9114cff69ad
wangshaocong <641152073@qq.com>
2026-08-11 09:48:00 +0800

    support opd training for qwen3Omni model

--- verl_omni/workers/rollout/vllm_rollout/vllm_omni_ar_strategy.py
+++ verl_omni/workers/rollout/vllm_rollout/vllm_omni_ar_strategy.py
@@ -24,6 +24,7 @@ import yaml
 from verl.utils.device import get_visible_devices_keyword
 from verl.workers.config import RolloutConfig
 from verl.workers.rollout.replica import TokenOutput
+from verl.workers.rollout.vllm_rollout.utils import extract_prompt_logprobs
 from verl.workers.rollout.vllm_rollout.vllm_async_server import vLLMHttpServer
 from vllm import SamplingParams
 from vllm_omni.lora.request import LoRARequest
@@ -372,6 +373,12 @@ class ARStrategy(OmniStrategyBase):
 
         extra_fields = {"global_steps": self.server.global_steps}
         extra_fields.update(rollout_fields)
+
+        extract_prompt_logprobs(
+            output=req_output,
+            num_prompt_logprobs=params.prompt_logprobs,
+            result_dict=extra_fields,
+        )
         token_ids = req_output.outputs[0].token_ids
         log_probs = None
         policy_params = params[self._policy_stage_index] if isinstance(params, list) else params
